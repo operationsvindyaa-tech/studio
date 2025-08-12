12a683e9867d5423d4b3807f71ff73f938ffe57a
OPERATIONS VINDYAA <operations.vindyaa@gmail.com>
2025-08-12 08:59:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,9 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <AppShell>{children}</AppShell>
+        <div>
+            <AppShell>{children}</AppShell>
+        </div>
         <Toaster />
       </body>
     </html>
