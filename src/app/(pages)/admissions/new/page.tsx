5fc77cd74c7159db08aa05586f046e7190adb466
Suresh Kumar <skk6090@gmail.com>
2025-08-12 06:35:25 +0000

    format of dob and date of joining to in dd mm yyyy in new admission appl

--- src/app/(pages)/admissions/new/page.tsx
+++ src/app/(pages)/admissions/new/page.tsx
@@ -193,14 +193,14 @@ export default function NewAdmissionPage() {
                             <FormField control={form.control} name="dateOfJoining" render={({ field }) => (
                                 <FormItem className="flex flex-col"><FormLabel>Date of Joining</FormLabel>
                                     <Popover><PopoverTrigger asChild>
-                                        <FormControl><Button variant={"outline"} className={cn("pl-3 text-left font-normal", !field.value && "text-muted-foreground")}>{field.value ? format(field.value, "PPP") : <span>Pick a date</span>}<CalendarIcon className="ml-auto h-4 w-4 opacity-50" /></Button></FormControl>
+                                        <FormControl><Button variant={"outline"} className={cn("pl-3 text-left font-normal", !field.value && "text-muted-foreground")}>{field.value ? format(field.value, "dd-MM-yyyy") : <span>Pick a date</span>}<CalendarIcon className="ml-auto h-4 w-4 opacity-50" /></Button></FormControl>
                                     </PopoverTrigger><PopoverContent className="w-auto p-0" align="start"><Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus /></PopoverContent></Popover><FormMessage />
                                 </FormItem>
                             )} />
                              <FormField control={form.control} name="dob" render={({ field }) => (
                                 <FormItem className="flex flex-col"><FormLabel>Date of Birth</FormLabel>
                                     <Popover><PopoverTrigger asChild>
-                                        <FormControl><Button variant={"outline"} className={cn("pl-3 text-left font-normal", !field.value && "text-muted-foreground")}>{field.value ? format(field.value, "PPP") : <span>Pick a date</span>}<CalendarIcon className="ml-auto h-4 w-4 opacity-50" /></Button></FormControl>
+                                        <FormControl><Button variant={"outline"} className={cn("pl-3 text-left font-normal", !field.value && "text-muted-foreground")}>{field.value ? format(field.value, "dd-MM-yyyy") : <span>Pick a date</span>}<CalendarIcon className="ml-auto h-4 w-4 opacity-50" /></Button></FormControl>
                                     </PopoverTrigger><PopoverContent className="w-auto p-0" align="start"><Calendar
                                         mode="single"
                                         captionLayout="dropdown-buttons"
