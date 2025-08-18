a677d6ec729ca1a337004bdbcc2ff4e3e8e59ef9
OPERATIONS VINDYAA <operations.vindyaa@gmail.com>
2025-08-18 08:13:55 +0000

    remove currency symbol under instructor and staff

--- src/app/(pages)/instructor-staff-report/page.tsx
+++ src/app/(pages)/instructor-staff-report/page.tsx
@@ -17,8 +17,6 @@ import { Users, BarChart, Wallet, Star } from "lucide-react";
 
 const formatCurrency = (amount: number) => {
     return new Intl.NumberFormat('en-IN', {
-        style: 'currency',
-        currency: 'INR',
         minimumFractionDigits: 0,
         maximumFractionDigits: 0,
     }).format(amount);
